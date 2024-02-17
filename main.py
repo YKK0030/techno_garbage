2c0df0412571f0ebad8c8e15073329a70588bc9d
YKK0030 <107952720+YKK0030@users.noreply.github.com>
2024-02-17 17:32:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,242 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Activation, BatchNormalization
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import classification_report,confusion_matrix
+from tensorflow import keras
+from PIL import Image
+from pathlib import Path
+import scipy
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as T
+import pandas as pd
+import seaborn as sns
+from sklearn import metrics
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras.utils import np_utils
+from sklearn.metrics import confusion_matrix
+
+data_dir = Path('Garbage\original_images')
+
+transformer = T.Compose([T.Resize((32, 32)), T.ToTensor()])
+dataset = ImageFolder(data_dir, transform = transformer)
+
+print(dataset.classes)
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+counts = [393,491,400,584,472,127]
+ax.bar(dataset.classes,counts)
+plt.title("Class distribution")
+plt.show()
+PATH_TEST = r"Garbage\original_images"
+PATH_TRAIN = r"Garbage\processed_images"
+class_names = ['cardboard', 'glass', 'metal','paper','plastic','trash']
+imagepath_cardboard = r"Garbage\original_images\cardboard"
+graypath_cardboard = r"Garbage\processed_images\cardboard"
+File_listing = os.listdir(imagepath_cardboard)
+for file in File_listing:
+    im = Image.open(imagepath_cardboard + '\\' + file) 
+    img = im.resize((32,32))
+    gray = img.convert('L')
+    gray.save(graypath_cardboard + '\\' + file, "JPEG")
+    
+imagepath_glass = r"Garbage\original_images\glass"
+graypath_glass = r"Garbage\processed_images\glass"
+File_listing = os.listdir(imagepath_glass)
+for file in File_listing:
+    im = Image.open(imagepath_glass + '\\' + file) 
+    img = im.resize((32,32))
+    gray = img.convert('L')
+    gray.save(graypath_glass + '\\' + file, "JPEG")
+imagepath_metal = r"Garbage\original_images\metal"
+graypath_metal = r"Garbage\processed_images\metal"
+File_listing = os.listdir(imagepath_metal)
+for file in File_listing:
+    im = Image.open(imagepath_metal + '\\' + file) 
+    img = im.resize((32,32))
+    gray = img.convert('L')
+    gray.save(graypath_metal + '\\' + file, "JPEG")
+imagepath_paper = r"Garbage\original_images\paper"
+graypath_paper = r"Garbage\processed_images\paper"
+File_listing = os.listdir(imagepath_paper)
+for file in File_listing:
+    im = Image.open(imagepath_paper + '\\' + file) 
+    img = im.resize((32,32))
+    gray = img.convert('L')
+    gray.save(graypath_paper + '\\' + file, "JPEG")
+imagepath_plastic = r"Garbage\original_images\plastic"
+graypath_plastic = r"Garbage\processed_images\plastic"
+File_listing = os.listdir(imagepath_plastic)
+for file in File_listing:
+    im = Image.open(imagepath_plastic + '\\' + file) 
+    img = im.resize((32,32))
+    gray = img.convert('L')
+    gray.save(graypath_plastic + '\\' + file, "JPEG")
+imagepath_trash = r"Garbage\original_images\trash"
+graypath_trash = r"Garbage\processed_images\trash"
+File_listing = os.listdir(imagepath_trash)
+for file in File_listing:
+    im = Image.open(imagepath_trash + '\\' + file) 
+    img = im.resize((32,32))
+    gray = img.convert('L')
+    gray.save(graypath_trash + '\\' + file, "JPEG")
+imagepath_cardboard_dir = os.path.join(imagepath_cardboard)
+imagepath_glass_dir = os.path.join(imagepath_glass)
+imagepath_metal_dir = os.path.join(imagepath_metal)
+imagepath_paper_dir = os.path.join(imagepath_paper)
+imagepath_plastic_dir = os.path.join(imagepath_plastic)
+imagepath_trash_dir = os.path.join(imagepath_trash)
+len(os.listdir(PATH_TRAIN))
+IMG_HEIGHT = 32
+IMG_WIDTH = 32
+image_gen = ImageDataGenerator(rescale=1./255)
+train_data_gen = image_gen.flow_from_directory(
+    directory = train_dir, 
+    shuffle=True, 
+    target_size = (IMG_HEIGHT, IMG_WIDTH),
+    class_mode='categorical')
+test_data_gen = image_gen.flow_from_directory(
+    directory = test_dir, 
+    shuffle=True, 
+    target_size = (IMG_HEIGHT, IMG_WIDTH),
+    class_mode='categorical')
+sample_data_gen = image_gen.flow_from_directory(
+    directory = test_dir, 
+    shuffle=True, 
+    target_size = (200, 200),
+    class_mode='categorical')
+
+sample_training_images, _= next(sample_data_gen)
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1,4, figsize=(30,30))
+    axes = axes.flatten()
+    for img, ax in zip(images_arr, axes):
+        ax.imshow(img)
+        ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+plotImages(sample_training_images[:4])
+model = Sequential([
+    Conv2D(filters=32, kernel_size=3, padding='same', activation='relu', input_shape=(IMG_HEIGHT,IMG_WIDTH, 3)),
+    MaxPooling2D(pool_size=2),
+
+    Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'),
+    MaxPooling2D(pool_size=2),
+    
+    Conv2D(filters=32, kernel_size=3, padding='same', activation='relu'),
+    MaxPooling2D(pool_size=2),
+    
+    Conv2D(filters=32, kernel_size=3, padding='same', activation='relu'),
+    MaxPooling2D(pool_size=2),
+
+    Flatten(),
+
+    Dense(6, activation='softmax')
+])
+
+import visualkeras
+visualkeras.layered_view(model, legend=True, spacing=20)
+
+num_cardboard_train = len(os.listdir(imagepath_cardboard_dir))
+num_glass_train = len(os.listdir(imagepath_glass_dir))
+num_metal_train = len(os.listdir(imagepath_metal_dir))
+num_paper_train = len(os.listdir(imagepath_cardboard_dir))
+num_plastic_train = len(os.listdir(imagepath_glass_dir))
+num_trash_train = len(os.listdir(imagepath_trash_dir))
+
+num_cardboard_test = len(os.listdir(graypath_cardboard))
+num_glass_test = len(os.listdir(graypath_glass))
+num_metal_test = len(os.listdir(graypath_metal))
+num_paper_test = len(os.listdir(graypath_paper))
+num_plastic_test = len(os.listdir(graypath_plastic))
+num_trash_test = len(os.listdir(graypath_trash))
+
+total_train = num_cardboard_train + num_glass_train + num_metal_train + num_paper_train + num_plastic_train + num_trash_train
+total_test = num_cardboard_test + num_glass_test + num_metal_test + num_paper_test + num_plastic_test + num_trash_test
+history = model.fit(
+    train_data_gen,
+    validation_data = train_data_gen,
+    steps_per_epoch= total_train // batch_size,
+    epochs = epochs,
+    validation_steps= total_test // batch_size,
+    callbacks = [tf.keras.callbacks.EarlyStopping(
+                monitor='val_loss',
+                min_delta=0.01,
+                patience=7)])
+test_loss, test_acc = model.evaluate(test_data_gen)
+print('Test accuracy: {} Test Loss: {} '.format(test_acc*100, test_loss))
+(X,y) = (train_data_gen[0], train_data_gen[1])
+y_test = train_test_split(X,y,test_size=0.2, random_state=4)
+
+nb_classes = 6
+Y_train = model.predict(train_data_gen)
+y_train = np.argmax(Y_train, axis=1)
+Y_test = model.predict(test_data_gen)
+y_test = np.argmax(Y_test, axis=1)
+cm = confusion_matrix(y_test, y_train)
+
+matrix = pd.DataFrame((metrics.confusion_matrix(y_test, y_train)), ('cardboard', 'glass', 'metal','paper','plastic','trash'),
+                      ('cardboard', 'glass', 'metal','paper','plastic','trash'))
+
+heatmap = sns.heatmap(matrix, annot = False, annot_kws = {'size': 14}, fmt = 'd', cmap = 'crest')
+heatmap.yaxis.set_ticklabels(heatmap.yaxis.get_ticklabels(), fontsize = 12)
+heatmap.xaxis.set_ticklabels(heatmap.xaxis.get_ticklabels(), fontsize = 12)
+plt.title('CNN Confusion Matrix', fontsize = 15, pad = 12, color = 'darkblue')
+plt.ylabel('True label', fontsize = 14)
+plt.xlabel('Predicted label', fontsize = 14)
+plt.show()
+
+print('Classification Report')
+target_names = ['cardboard', 'glass', 'metal','paper','plastic','trash']
+print(classification_report(train_data_gen.classes, y_test, target_names=target_names,zero_division=0))
+
+FP = cm.sum(axis=0) - np.diag(cm)  
+FN = cm.sum(axis=1) - np.diag(cm)
+TP = np.diag(cm)
+TN = cm.sum() - (FP + FN + TP)
+
+print ("Cardboard Glass Metal Paper Plastic Trash")
+ACC = (TP+TN)/(TP+FP+FN+TN)
+print (ACC)
+
+from skimage.transform import resize 
+
+import keras.utils as ku
+import numpy as np
+number_to_class = ['cardboard',\
+                   'glass',\
+                   'metal',
+                   'paper',\
+                   'plastic',\
+                   'trash',]
+
+test_x, test_y = train_data_gen.__getitem__(1)
+# test_x is used to train the data
+# test_y refers to the actual answer of the testing set data
+# preds refers to the predicted class
+preds = model.predict(test_x)
+
+plt.figure(figsize=(20, 20))
+for i in range(12):
+    plt.subplot(4,4, i+1)
+    plt.title('pred:%s / truth:%s' % (number_to_class[np.argmax(preds[i])], number_to_class[np.argmax(test_y[i])]), size=18)
+    plt.imshow(test_x[i])
+    
+test_img = 'Garbage/processed_images/glass/glass_341.jpg'
+img = ku.load_img(test_img, target_size = (32,32))
+img = ku.img_to_array(img, dtype=np.uint8)
+img = np.array(img)/255.0
+prediction = model.predict(img[np.newaxis, ...])
+
+print("Probability:",np.max(prediction[0], axis=-1))
+predicted_class = number_to_class[np.argmax(prediction[0], axis=-1)]
+print("Classified:",predicted_class,'\n')
+
+plt.axis('off')
+plt.imshow(img.squeeze())
+plt.title("Loaded Image")
